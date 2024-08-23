0ddd1cfad4ebd61c94b557b13faed9feb2a085f4
John Rucana <hillsong123456789@gmail.com>
2024-08-22 19:54:29 -0500

    if CONDICIONALES

---
+++ script.js
@@ -0,0 +1,28 @@
+/* CONDICIONALES IF */
+let condicion=7
+if (condicion==1) {
+    console.log("Es verdadero")
+} else {
+    console.log("Es falso")
+}
+
+/* CONDICIONALES IF MÚLTIPLES */
+if (10<3) {
+    console.log("1")
+} else if (1>5) {
+    console.log("2")
+} else if (6==7) {
+    console.log("3")
+} else {
+    console.log("4")
+}
+
+
+if (12==12) {
+    console.log("1")
+    if (13>5) {
+        console.log("si voy")
+    }
+} else {
+    console.log("2")
+}
