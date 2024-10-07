74b287b56515ab92c9d6e7f49d7c907816723b1a
John Rucana <hillsong123456789@gmail.com>
2024-10-06 19:21:05 -0500

    Ejericicios for completos

--- ejerfor.js
+++ ejerfor.js
@@ -88,3 +88,75 @@ for(i=1; i<=numero; i++){
     cadena = cadena + letra
 }
 console.log(cadena)*/
+
+
+
+/*
+Se quiere tener un contador de dos dígitos en base 5. El funcionamiento es simple:
+el dígito de la derecha irá aumentando y cuando pase de 4 su valor se pondrá a 0 y
+el dígito de la izquierda se incrementa en 1. El contador debe deternerse cuando
+alcancemos el valor 1:4
+*/
+/*for(let j=0;j<=2;j++){
+    for(let i=0;i<=4;i++){
+        console.log(j+" : "+i)
+    }
+}*/
+
+/*let i=0
+while (i<=2) {
+    let j=1
+    while (j<=4) {
+      console.log(i+" : "+j)
+      j++  
+    }
+    i++
+}*/
+
+
+
+/*
+Se trata de dibujar un triángulo rectángulo con asteriscos. El usuario tecleará un
+valor entero, el programa escribirá con asteriscos tantas lineas como diga ese número.
+Cada linea estará formada por una serie de astericos tan larga como diga el número de línea en el que está.
+
+Para separar una linea de la siguiente en console o en alert debes usar "\n". En este ejercicio usa console.log.
+*/
+
+
+/*let numero=parseInt(prompt("Ingrese número: "))
+let cadena1=""
+for(let i=1;i<=numero;i++){
+    let cadena2=""
+    for(let j=1;j<=i;j++){
+        cadena2=cadena2+" *"
+    }
+     cadena1=cadena1+cadena2+"\n"
+}
+alert(cadena1)*/
+
+
+/*
+Se trata de crear una aplicación que calcule la suma de todos los números enteros positivos entre 0 y
+el número tecleado por el usuario. El resultado será de la forma "Los números enteros de 0 a n  suman suma"
+*/
+
+/*let numero=parseInt(prompt("Ingrese numero: "))
+let suma=0
+for(i=0;i<=numero;i++){
+    suma =suma+i
+}
+alert(suma)*/
+
+
+
+/*
+Programa un script que pida al usuario que teclee 4 números (uno cada vez). Con esos números el programa
+deberá calcular la media aritmética. O sea la suma de los 4 números dividida entre 4
+*/
+let numero,suma=0
+for(let i=1;i<=4;i++){
+    numero=parseInt(prompt("Inresese número: "))
+    suma=suma+numero  
+}
+alert(suma/4)
